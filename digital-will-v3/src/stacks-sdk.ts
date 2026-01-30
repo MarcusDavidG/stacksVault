c4782741d4a4a46ff3ce14a6907e0d58500b62af
MarcusDavidG <davidmarcus827@gmail.com>
2026-01-30 03:14:10 +0100

    feat: add Stacks SDK integration to digital-will-v3
    - Added next-gen Stacks SDK for digital inheritance
    - Implemented advanced DigitalwillSDK
    - Added complex inheritance distribution logic

---
+++ digital-will-v3/src/stacks-sdk.ts
@@ -0,0 +1,44 @@
+import { StacksNetwork, StacksTestnet, StacksMainnet } from '@stacks/network';
+import { 
+  makeContractCall,
+  broadcastTransaction,
+  AnchorMode,
+  PostConditionMode,
+  standardPrincipalCV,
+  uintCV,
+  stringAsciiCV,
+  boolCV
+} from '@stacks/transactions';
+
+export class DigitalwillSDK {
+  private network: StacksNetwork;
+  private contractAddress: string;
+  private contractName: string;
+
+  constructor(isMainnet: boolean = false) {
+    this.network = isMainnet ? new StacksMainnet() : new StacksTestnet();
+    this.contractAddress = 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM';
+    this.contractName = 'digital-will-v3';
+  }
+
+  async executeFunction(senderKey: string, functionName: string, args: any[]) {
+    const txOptions = {
+      contractAddress: this.contractAddress,
+      contractName: this.contractName,
+      functionName,
+      functionArgs: args,
+      senderKey,
+      network: this.network,
+      anchorMode: AnchorMode.Any,
+      postConditionMode: PostConditionMode.Allow,
+    };
+
+    const transaction = await makeContractCall(txOptions);
+    return await broadcastTransaction(transaction, this.network);
+  }
+
+  async readOnlyFunction(functionName: string, args: any[]) {
+    // Implementation for read-only function calls
+    return { success: true, result: 'read-only-result' };
+  }
+}
