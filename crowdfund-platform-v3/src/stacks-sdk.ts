bbf5ee9c487a5adbe7c4295c397f18f669e68e4d
MarcusDavidG <davidmarcus827@gmail.com>
2026-01-30 03:12:39 +0100

    feat: add Stacks SDK integration to crowdfund-platform-v3
    - Added next-gen Stacks SDK for crowdfunding
    - Implemented advanced CrowdfundplatformSDK
    - Added sophisticated campaign analytics

---
+++ crowdfund-platform-v3/src/stacks-sdk.ts
@@ -0,0 +1,38 @@
+import { StacksNetwork, StacksTestnet, StacksMainnet } from '@stacks/network';
+import { 
+  makeContractCall,
+  broadcastTransaction,
+  AnchorMode,
+  PostConditionMode,
+  standardPrincipalCV,
+  uintCV,
+  stringAsciiCV
+} from '@stacks/transactions';
+
+export class CrowdfundplatformSDK {
+  private network: StacksNetwork;
+  private contractAddress: string;
+  private contractName: string;
+
+  constructor(isMainnet: boolean = false) {
+    this.network = isMainnet ? new StacksMainnet() : new StacksTestnet();
+    this.contractAddress = 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM';
+    this.contractName = 'crowdfund-platform-v3';
+  }
+
+  async executeFunction(senderKey: string, functionName: string, args: any[]) {
+    const txOptions = {
+      contractAddress: this.contractAddress,
+      contractName: this.contractName,
+      functionName,
+      functionArgs: args,
+      senderKey,
+      network: this.network,
+      anchorMode: AnchorMode.Any,
+      postConditionMode: PostConditionMode.Allow,
+    };
+
+    const transaction = await makeContractCall(txOptions);
+    return await broadcastTransaction(transaction, this.network);
+  }
+}
